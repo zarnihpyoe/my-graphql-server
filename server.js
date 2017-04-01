ddf91629a2daabb57704bc84f7650f36be7417e5
Zarni Phyo <zarniphyo95@gmail.com>
2017-04-01 14:30:02 +0630

    stage1: Test a query with graphiql

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
-console.log({ starting: true })
-
 import express from 'express'
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql'
+import graphqlHTTP from 'express-graphql'
 
 const app = express()
 
-app.use('/graphql', (req, res) => {
-  res.send({ data: true })
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQuery',
+  description: 'The root query',
+  fields: {
+
+    viewer: {
+      type: GraphQLString,
+      resolve() {
+        return 'viewer!'
+      }
+    },
+
+  }
 })
+
+const Schema = new GraphQLSchema({
+  query: RootQuery
+})
+
+app.use('/graphql', graphqlHTTP({
+  schema: Schema,
+  graphiql: true,
+}))
 
 app.listen(3000, () => {
   console.log({ running: true })
